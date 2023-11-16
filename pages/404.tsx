36818c61d8a40987ef39ea44effee08ece1149ed
Jake Simian <jake.simian@keble.ox.ac.uk>
2023-11-16 12:44:48 +0000

    Add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,13 @@
+const PageNotFound = () => {
+  return (
+    <>
+      <h1>Page Not Found!</h1>
+      <p>
+        Sorry, the page you were looking for is not here. Try returning to the
+        <a href="/">home</a> page.
+      </p>
+    </>
+  );
+};
+
+export default PageNotFound;
