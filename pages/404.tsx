2d9668ecb0baaf4b95406a7dc59aef488eab2c3e
Jake Simian <jake.simian@keble.ox.ac.uk>
2023-11-16 12:48:28 +0000

    Fix Link

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 const PageNotFound = () => {
   return (
     <>
       <h1>Page Not Found!</h1>
       <p>
         Sorry, the page you were looking for is not here. Try returning to the
-        <a href="/">home</a> page.
+        <Link href="/">home</Link> page.
       </p>
     </>
   );
