da67a49e2c8561a556955031a21afd08638da3db
Jessica Simian <jessica.simian@keble.ox.ac.uk>
2024-06-10 00:26:35 +0100

    Upgrade next, react, adds vercel insights

--- components/Head.tsx
+++ components/Head.tsx
@@ -7,84 +7,84 @@ import Link from "next/link";
 import styles from "../styles/Head.module.css";
 
 const Head = ({ nav }: { nav: boolean }) => {
-  const [opacity, setOpacity] = useState(0.0);
-  return (
-    <div
-      style={{
-        width: "90vw",
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-      }}
-    >
-      <div
-        onClick={(_) => Router.push("/")}
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(2, 1fr)",
-          position: "relative",
-        }}
-      >
-        <div style={{ gridColumn: 1, gridRow: 1, zIndex: 1 }}>
-          <Image
-            alt="Website Logo"
-            onMouseEnter={(_) => {
-              setOpacity(1.0);
-            }}
-            onMouseLeave={(_) => {
-              setOpacity(0.0);
-            }}
-            src="/saturn_color.png"
-            width="100px"
-            height="100px"
-            style={{
-              opacity: opacity,
-              position: "absolute",
-              top: "0px",
-              left: "0px",
-              transition: "opacity 0.5s ease",
-            }}
-          />
-        </div>
-        <div style={{ gridColumn: 1, gridRow: 1, zIndex: 0 }}>
-          <Image
-            alt="Website Logo"
-            src="/saturn.png"
-            width="100px"
-            height="100px"
-            style={{
-              opacity: 1.0 - opacity,
-              position: "absolute",
-              top: "0px",
-              left: "-100px",
-              transition: "opacity 0.5s ease",
-            }}
-          />
-        </div>
-      </div>
-      {nav ? (
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "end",
-            maxWidth: "250px",
-          }}
-        >
-          <Link
-            className={styles.nav_link}
-            href="/about"
-            style={{ textAlign: "right" }}
-          >
-            About
-          </Link>
-        </div>
-      ) : (
-        <></>
-      )}
-    </div>
-  );
+	const [opacity, setOpacity] = useState(0.0);
+	return (
+		<div
+			style={{
+				width: "90vw",
+				display: "flex",
+				flexDirection: "row",
+				justifyContent: "space-between",
+				alignItems: "center",
+			}}
+		>
+			<div
+				onClick={(_) => Router.push("/")}
+				style={{
+					display: "grid",
+					gridTemplateColumns: "repeat(2, 1fr)",
+					position: "relative",
+					width: "100px"
+				}}
+			>
+				<div style={{ gridColumn: 1, gridRow: 1, zIndex: 1, width: "100px", height: "100px" }}>
+					<Image
+						alt="Website Logo"
+						onMouseEnter={(_) => {
+							setOpacity(1.0);
+						}}
+						onMouseLeave={(_) => {
+							setOpacity(0.0);
+						}}
+						src="/saturn_color.png"
+						width={100}
+						height={100}
+						style={{
+							opacity: opacity,
+							position: "absolute",
+							top: "0px",
+							left: "0px",
+							transition: "opacity 0.5s ease",
+						}}
+					/>
+				</div>
+				<div style={{ gridColumn: 1, gridRow: 1, zIndex: 0, width: "100px", height: "100px" }}>
+					<Image
+						alt="Website Logo"
+						src="/saturn.png"
+						width={100}
+						height={100}
+						style={{
+							opacity: 1.0 - opacity,
+							position: "absolute",
+							top: "0px",
+							transition: "opacity 0.5s ease",
+						}}
+					/>
+				</div>
+			</div>
+			{nav ? (
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "row",
+						justifyContent: "end",
+						maxWidth: "250px",
+					}}
+				>
+					<Link
+						className={styles.nav_link}
+						href="/about"
+						style={{ textAlign: "right" }}
+					>
+						About
+					</Link>
+				</div>
+			) : (
+				<></>
+			)}
+		</div>
+	);
 };
 
 export default Head;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,22 +5,23 @@ import { Analytics } from "@vercel/analytics/react";
 import "../styles/globals.css";
 import { useRouter } from "next/router";
 import { initialiseFirebase } from "../lib/libFirebase";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const { app: firebaseApp, storage: firebaseStorage } =
-  initialiseFirebase();
+	initialiseFirebase();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
+	const router = useRouter();
 
-  const navPages = ["/", "/now", "/links", "/about", "/uses"];
+	const navPages = ["/", "/now", "/links", "/about", "/uses"];
 
-  return (
-    <>
-      <Head nav={navPages.includes(router.asPath)} />
-      <Component {...pageProps} />
-      <Analytics />
-    </>
-  );
+	return (
+		<>
+			<Head nav={navPages.includes(router.asPath)} />
+			<Component {...pageProps} />
+			<Analytics />
+		</>
+	);
 }
 
 export default MyApp;
