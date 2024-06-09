3f2911bbf368961a059bfe42522aa8bf7335daed
Jessica Simian <jessica.simian@keble.ox.ac.uk>
2024-06-09 19:12:09 +0100

    Added new article

--- pages/articles/[id].tsx
+++ pages/articles/[id].tsx
@@ -3,41 +3,42 @@ import { GetStaticPaths, GetStaticProps } from "next";
 import Link from "next/link";
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const postData = await getPostData(params!.id as string);
-  return { props: { postData } };
+	const postData = await getPostData(params!.id as string);
+	return { props: { postData } };
 };
 
 export const getStaticPaths: GetStaticPaths<{ id: string }> = async () => {
-  const ids = await getPostList();
-  let paths: any = ids.map((x: string) => ({ params: { id: x } }));
-  return { paths, fallback: "blocking" };
+	const ids = await getPostList();
+	let paths: any = ids.map((x: string) => ({ params: { id: x } }));
+	return { paths, fallback: "blocking" };
 };
 
 export default function Article({ postData }: { postData: Post }) {
-  return (
-    <>
-      <header className="header">
-        <h1>{postData.metadata.title}</h1>
-        <p className="datePublished">
-          {" "}
-          First published on: {postData.metadata.dateCreated}{" "}
-        </p>
-        <p className="tags">
-          Tags:
-          {postData.metadata.tags.map((x: string, i) => (
-            <Tag tagName={x} key={i} />
-          ))}
-        </p>
-      </header>
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-    </>
-  );
+	return (
+		<>
+			<header className="header">
+				<h1>{postData.metadata.title}</h1>
+				<p className="datePublished">
+					{" "}
+					First published on: {postData.metadata.dateCreated}{" "}
+				</p>
+				<p className="tags">
+					Tags:
+					{postData.metadata.tags.map((x: string, i) => (
+						<Tag tagName={x} key={i} />
+					))}
+				</p>
+			</header>
+			<div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+				dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+		</>
+	);
 }
 
 export const Tag = ({ tagName }: { tagName: string }) => {
-  return (
-    <code className="tagElement" style={{ marginBottom: 0 }}>
-      <Link href={`/?tags=${tagName}`}>{`#${tagName}`}</Link>
-    </code>
-  );
+	return (
+		<code className="tagElement" style={{ marginBottom: 0 }}>
+			<Link href={`/?tags=${tagName}`}>{`#${tagName}`}</Link>
+		</code>
+	);
 };
